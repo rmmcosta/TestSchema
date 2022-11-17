81562e522304a0c2eaff24dbf66f8d77fe3bc6b3
Ricardo Costa <ricardocosta101085@gmail.com>
2022-11-17 21:37:59 +0000

    first commit. Simple SpringBoot project with a test on using spring.datasource.platform, but is not working.

---
+++ src/main/java/com/testschema/testschema/TestschemaApplication.java
@@ -0,0 +1,13 @@
+package com.testschema.testschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestschemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestschemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testschema/testschema/TestschemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testschema.testschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestschemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
